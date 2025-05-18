c8d784c5b2b9243230284ce3f347d122b04af855
Artem Kudyakov <t3m8ch@yandex.ru>
2025-05-18 15:52:56 +0400

    feat: Add wasmer

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use wasmer::{Instance, Module, Store, Value, imports};
+
+fn main() -> anyhow::Result<()> {
+    let module_wat = r#"
+    (module
+    (type $t0 (func (param i32) (result i32)))
+    (func $add_one (export "add_one") (type $t0) (param $p0 i32) (result i32)
+        local.get $p0
+        i32.const 1
+        i32.add))
+    "#;
+
+    let mut store = Store::default();
+    let module = Module::new(&store, &module_wat)?;
+    let import_object = imports! {};
+    let instance = Instance::new(&mut store, &module, &import_object)?;
+
+    let add_one = instance.exports.get_function("add_one")?;
+    let result = add_one.call(&mut store, &[Value::I32(42)])?;
+    println!("Result: {:?}", result);
+    assert_eq!(result[0], Value::I32(43));
+
+    Ok(())
 }
